667ca834053491290a66d2923220d6b64dd59881
Lucas <lucasmorais582@gmail.com>
2021-01-13 00:16:57 -0300

    Retirada de informações pessoais

--- src/database/index.ts
+++ src/database/index.ts
@@ -8,7 +8,7 @@ export default async (name = 'default'): Promise<Connection> => {
       name,
       database:
         process.env.NODE_ENV === 'test'
-          ? 'desafio_winnin'
+          ? 'coloque o nome do banco criado aqui'
           : defaultOptions.database,
     }),
   );
